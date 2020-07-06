267cc425504b4f39241aff4a41055d7d30bc107b
Vishwas B <bvishwas21@gmail.com>
2020-07-06 18:01:56 +0530

    Changes path

--- es6.js
+++
@@ -1,9 +0,0 @@
-// ES6 - ECMAScript 6
-// Converted to ES5 by many browsers using babel
-
-//-------------------------------------------------------------------
-// Temporal Deadzone Error (TDZ)
-// console.log(a);
-// let a = 10;          // This will not be hoisted to the top
-//-------------------------------------------------------------------
-
