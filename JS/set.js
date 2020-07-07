592a25c18d37af2a25534b18af5e93d92550fb99
B <visb@lowes.com>
2020-07-07 14:44:12 +0530

    Day Two - Mid Day Commit

---
+++ JS/set.js
@@ -0,0 +1,19 @@
+{
+    "terminal.integrated.shell.windows": "C:\\WINDOWS\\System32\\cmd.exe",
+    "python.jediEnabled": false,
+    "editor.formatOnSave": true,
+    "python.pythonPath": "‪C:\\Python38-32\\python.exe",
+    "python.languageServer": "Microsoft",
+    "prettier.jsxSingleQuote": true,
+    "prettier.endOfLine": "auto",
+    "prettier.arrowParens": "avoid",
+    "javascript.updateImportsOnFileMove.enabled": "always",
+    "[javascript]": {
+      "editor.defaultFormatter": "esbenp.prettier-vscode"
+    },
+    "emmet.includeLanguages": {
+      "javascript": "javascriptreact"
+    },
+    "prettier.tabWidth": 4,
+  }
+  
