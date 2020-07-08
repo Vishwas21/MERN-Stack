9771965fda34f99fdd7d3a0d1e61591891d854f3
Vishwas B <bvishwas21@gmail.com>
2020-07-08 11:19:58 +0530

    New Push

---
+++ courseProject/routes/apis/posts.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { PromiseProvider } = require("mongoose");
+const router = express.Router();
+
+// @route GET api/posts
+// @desc  test route
+// @access  private route
+
+router.get("/");
