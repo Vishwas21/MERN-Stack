fd27460010231024e768c1814d53a75c2851cddc
B <visb@lowes.com>
2020-07-08 15:17:12 +0530

    Push

--- courseProject/routes/apis/user.js
+++ courseProject/routes/apis/user.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const { check, validationResult } = require("express-validator");
 const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const gravatar = require("gravatar");
 
 // @route POST api/user
 // @desc  test route
@@ -13,7 +15,7 @@ router.post(
         check("name", "Name is required").not().isEmpty(),
         check("email", "We are familiar with email syntax, are we?").isEmail(),
         check(
-            "password",
+            "pass",
             "Hey, lets enter a more secure passsword, 6 characters or more"
         ).isLength({ min: 6 }),
     ],
@@ -28,18 +30,34 @@ router.post(
             // Check if the user exists
             const { name, email, pass } = req.body;
 
-            const user = await User.findOne({ email });
-            console.log("User : ", user);
+            let user = await User.findOne({ email });
             if (user) {
                 return res.status(400).json({
                     message:
                         "User with this email already exists. Please Check!",
                 });
             }
+            // Generate a gravatar url
+            const avatar = gravatar.url(email, {
+                s: "200",
+                r: "g",
+                d: "mm",
+            });
+
+            // Initialize User
+            user = new User({
+                name,
+                email,
+                pass,
+                avatar,
+            });
 
             // Encrypt the user's password
+            const salt = await bcrypt.genSalt(10);
+            user.pass = await bcrypt.hash(user.pass, salt);
 
             // Create the User
+            user.save();
 
             // Generate JWT token and send it back
         } catch (err) {
