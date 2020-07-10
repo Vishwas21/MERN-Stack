efaf9df889ea56cd37eb01a81d4709ade2627157
B <visb@lowes.com>
2020-07-10 19:48:02 +0530

    Day Five Day End

--- courseProject/client/src/App.js
+++ courseProject/client/src/App.js
@@ -69,7 +69,9 @@ class App extends Component {
                                 </Auth>
                             )}
                         />
-                        <Route exact path='/profile' component={Profile} />
+                        <Route exact path='/profile'>
+                            <Profile />
+                        </Route>
                     </Switch>
                 </Grid>
             </Router>
